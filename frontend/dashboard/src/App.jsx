99ddd17ba7fc97c42018ba6b72648760614d5e7d
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-23 16:08:22 +0330

    fixed react-router-dom and grid content-web

--- frontend/dashboard/src/App.jsx
+++ frontend/dashboard/src/App.jsx
@@ -1,7 +1,16 @@
+import { useRoutes } from "react-router-dom"
+import routes from "./routes"
 
 function App() {
+  const router = useRoutes(routes);
   return (
-    <div>App</div>
+    <div className="container">
+        <div className="container_sidebar">this is sidebar</div>
+        <div className="container_content">
+          {router}
+          this is content page
+          </div>
+    </div>
   )
 }
 
