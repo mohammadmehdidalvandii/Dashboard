152b05e487ec9c6848e336cbda82e74ec6e2f2c6
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-30 13:39:33 +0330

    created page Orders

--- frontend/dashboard/src/routes.jsx
+++ frontend/dashboard/src/routes.jsx
@@ -1,9 +1,11 @@
 import Home from "./page/Home/Home";
 import Products from "./page/Products/Products";
+import Orders from "./page/Orders/Orders";
 
 const routes = [
     {path:"/" , element:<Home/>},
-    {path:"/Products" , element:<Products/>}
+    {path:"/Products" , element:<Products/>},
+    {path:"/Orders" , element:<Orders/>},
 ]
 
 export default routes
