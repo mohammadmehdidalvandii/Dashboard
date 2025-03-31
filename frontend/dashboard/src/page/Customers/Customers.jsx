5dc277b7480abb4d5c9258fcb8a1ccc7d8384d5f
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-31 14:30:56 +0330

    created page Customers

---
+++ frontend/dashboard/src/page/Customers/Customers.jsx
@@ -0,0 +1,11 @@
+import MetaTag from '../../hook/MetaTag/MetaTag'
+
+function Customers() {
+  return (
+    <>
+    <MetaTag  title="Dashboard | Customers" />
+    </>
+  )
+}
+
+export default Customers
--- frontend/dashboard/src/routes.jsx
+++ frontend/dashboard/src/routes.jsx
@@ -2,12 +2,14 @@ import Home from "./page/Home/Home";
 import Products from "./page/Products/Products";
 import Orders from "./page/Orders/Orders";
 import OrderView from "./page/OrderView/OrderView";
+import Customers from "./page/Customers/Customers";
 
 const routes = [
     {path:"/" , element:<Home/>},
     {path:"/Products" , element:<Products/>},
     {path:"/Orders" , element:<Orders/>},
     {path:"/OrdersView" , element:<OrderView/>},
+    {path:"/Customers" , element:<Customers/>},
 ]
 
 export default routes
