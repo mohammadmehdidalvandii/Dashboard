b35be94150eca96394534bb9f401e4d7405deaf9
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-03-31 14:52:50 +0330

    add 2 lang for CustomersManagement

--- frontend/dashboard/src/components/templates/customers/CustomersManagement/CustomersManagement.jsx
+++ frontend/dashboard/src/components/templates/customers/CustomersManagement/CustomersManagement.jsx
@@ -1,20 +1,22 @@
 import { FaPlus } from 'react-icons/fa';
 import './CustomersManagement.css';
+import { useTranslation } from 'react-i18next';
 
 function CustomersManagement() {
+    const {t} = useTranslation();
   return (
    <section className="customersManagement box">
         <div>
             <div className="management">
                 <div>
-                    <h2>Customers Management</h2>
-                    <p>View and manage customers</p>
+                    <h2>{t("Customers Management")}</h2>
+                    <p>{t("View and manage customers")}</p>
                 </div>
                 <button className="btn">
                                 <span className="icon">
                                     <FaPlus/>
                                 </span>
-                                <span className="text">Add New Customer</span>
+                                <span className="text">{t("Add New Customer")}</span>
                                 </button>
             </div>
         </div>
