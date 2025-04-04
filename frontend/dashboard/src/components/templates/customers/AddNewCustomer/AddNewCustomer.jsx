13d273a20ac2c52afe67841549ba29e520e07de1
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-04 16:13:32 +0330

    add 2 lang for AddNewCustomers and DetailsCustomers

--- frontend/dashboard/src/components/templates/customers/AddNewCustomer/AddNewCustomer.jsx
+++ frontend/dashboard/src/components/templates/customers/AddNewCustomer/AddNewCustomer.jsx
@@ -1,50 +1,51 @@
 import './AddNewCustomer.css'
 import {NavLink} from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 function AddNewCustomer() {
+    const {t} =useTranslation();
   return (
     <section className="addCustomer box">
         <div>
-            <NavLink to='/Customers' className="btn link btn_back">Back To Customers</NavLink>
-            <h2 className='title_header'> Add New Customer</h2>
+            <NavLink to='/Customers' className="btn link btn_back">{t("Back To Customers")}</NavLink>
+            <h2 className='title_header'>{t("Add New Customer")}</h2>
             <form action="#" className="addCustomer_form">
                 <div className="form_wrapper">
                     <div className="from_group">
-                        <label>First Name</label>
+                        <label>{t("First Name")}</label>
                         <input type="text" className="form_input" />
                     </div>
                     <div className="from_group">
-                        <label>Last Name</label>
+                        <label>{t("Last Name")}</label>
                         <input type="text" className="form_input" />
                     </div>
                 </div>
                     <div className="from_group">
-                        <label>Email</label>
+                        <label>{t("Email")}</label>
                         <input type="text" className="form_input" />
                     </div>
                     <div className="from_group">
-                        <label>Phone</label>
+                        <label>{t("Phone")}</label>
                         <input type="text" className="form_input" />
                     </div>
                     <div className="from_group">
-                        <label>Address</label>
+                        <label>{t("Address")}</label>
                         <textarea type="text" className="form_input"  rows={4}/>
                     </div>
                     <div className="form_group">
                         <select className='form_input'>
-                            <option value="">Bronze</option>
-                            <option value="">Silver</option>
-                            <option value="">Gold</option>
-                            <option value="">PLatinum</option>
+                            <option value="">{t("Bronze")}</option>
+                            <option value="">{t("Silver")}</option>
+                            <option value="">{t("Gold")}</option>
+                            <option value="">{t("PLatinum")}</option>
                         </select>
                     </div>
                     <div className="from_group">
-                        <label>Note</label>
+                        <label>{t("Note")}</label>
                         <textarea type="text" className="form_input"  rows={4}/>
                     </div>
                     <div className="btn_action">
-                        <button className="btn">Save Customer</button>
-                        <button className="btn_cancel">Cancel</button>
+                        <button className="btn">{t("Save Customer")}</button>
                     </div>
             </form>
         </div>
--- frontend/dashboard/src/components/templates/customers/DetailsCustomer/DetailsCustomer.jsx
+++ frontend/dashboard/src/components/templates/customers/DetailsCustomer/DetailsCustomer.jsx
@@ -1,69 +1,71 @@
 import './DetailsCustomer.css';
 import {NavLink} from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 function DetailsCustomer() {
+  const {t} = useTranslation();
   return (
     <section className="detailsCustomer box">
         <div>
-          <NavLink to='/Customers' className='btn link btn_back'>Back To Customers</NavLink>
-          <h2 className="title_header">Customer Details</h2>
+          <NavLink to='/Customers' className='btn link btn_back'>{t("Back To Customers")}</NavLink>
+          <h2 className="title_header">{t("Customer Details")}</h2>
           <div className="items">
             <ul className="item">
               <li>
-                <h6>Personal Information</h6>
+                <h6>{t("Personal Information")}</h6>
               </li>
               <li>
-                <span>Customer ID:</span>
+                <span>{t("Customer ID")}:</span>
                 <span>C001</span>
               </li>
               <li>
-                <span>Name:</span>
+                <span>{t("Name")}:</span>
                 <span>John Doe</span>
               </li>
               <li>
-                <span>Email:</span>
+                <span>{t("Email")}:</span>
                 <span> john@example.com</span>
               </li>
               <li>
-                <span>Phone:</span>
+                <span>{t("Phone")}:</span>
                 <span>+1 234 567 890</span>
               </li>
               <li>
-                <span>Membership:</span>
+                <span>{t("Membership")}:</span>
                 <span>gold</span>
               </li>
             </ul>
             <ul className="item">
               <li>
-                <h6>Order Statistics</h6>
+                <h6>{t("Order Statistics")}</h6>
               </li>
               <li>
-                <span>Total Orders:</span>
+                <span>{t("Total Orders")}:</span>
                 <span>15</span>
               </li>
               <li>
-                <span>Total Spent: </span>
+                <span>{t("Total Spent")}: </span>
                 <span>$2499.9</span>
               </li>
               <li>
-                <span>Average Order Value:</span>
+                <span>{t("Average Order Value")}:</span>
                 <span>166.67</span>
               </li>
               <li>
-                <span>Last Order:</span>
+                <span>{t("Last Order")}:</span>
                 <span>4/4/2025</span>
               </li>
             </ul>
           </div>
-          <h2 className="title_header">Recent Orders</h2>
+          <h2 className="title_header">{t("Recent Orders")}</h2>
           <table>
               <thead>
                 <tr>
-                  <th>Order ID</th>
-                  <th>Date</th>
-                  <th>Items</th>
-                  <th>Total</th>
-                  <th>Status</th>
+                  <th>{t("Order ID")}</th>
+                  <th>{t("Date")}</th>
+                  <th>{t("Items")}</th>
+                  <th>{t("Total")}</th>
+                  <th>{t("Status")}</th>
                 </tr>
               </thead>
               <tbody>
