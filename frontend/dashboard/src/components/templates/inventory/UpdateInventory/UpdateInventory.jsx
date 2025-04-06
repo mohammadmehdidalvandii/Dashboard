c2be47f2c505328f8d94ccbc2e75aaaae3125906
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-06 14:19:17 +0330

    created section UpdateInventory for Inventory-page

--- frontend/dashboard/src/components/templates/inventory/UpdateInventory/UpdateInventory.jsx
+++ frontend/dashboard/src/components/templates/inventory/UpdateInventory/UpdateInventory.jsx
@@ -1,15 +1,17 @@
 import './UpdateInventory.css';
 import {NavLink} from 'react-router-dom'
+import {useTranslation} from 'react-i18next'
 
 function UpdateInventory() {
+    const {t} = useTranslation()
   return (
     <section className="inventoryUpdate box">
         <div>
-            <NavLink to='/Inventory' className="btn link btn_back">Back to Inventory</NavLink>
-            <h2 className="title_header">Update Stock</h2>
+            <NavLink to='/Inventory' className="btn link btn_back">{t("Back to Inventory")}</NavLink>
+            <h2 className="title_header">{t("Update Stock")}</h2>
             <form action="#" className="inventoryUpdate_form">
                 <div className="form_group">
-                    <label>Product Name</label>
+                    <label>{t("Product Name")}</label>
                     <input type="text" className="form_input" placeholder=''/>
                 </div>
                 <div className="form_group">
@@ -17,22 +19,22 @@ function UpdateInventory() {
                     <input type="text" className="form_input" placeholder=''/>
                 </div>
                 <div className="form_group">
-                    <label>Current Stock</label>
+                    <label>{t("Current Stock")}</label>
                     <input type="text" className="form_input" placeholder=''/>
                 </div>
                 <div className="form_group">
-                    <label>Add/Remove Stock</label>
+                    <label>{t("Add/Remove Stock")}</label>
                     <input type="text" className="form_input" placeholder=''/>
                 </div>
                 <div className="form_group">
-                    <label>New Stock Level</label>
+                    <label>{t("New Stock Level")}</label>
                     <input type="text" className="form_input" placeholder=''/>
                 </div>
                 <div className="form_group">
-                    <label>Note</label>
+                    <label>{t("Note")}</label>
                     <textarea type="text" rows={4} className="form_input" placeholder=''/>
                 </div>
-                <button className="btn">Update Stock</button>
+                <button className="btn">{t("Update Stock")}</button>
             </form>
         </div>
     </section>
