7e13059c3daac0a3eb59c658e7cb2e72410f9613
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-06 14:25:27 +0330

    created page Settings

---
+++ frontend/dashboard/src/page/Settings/Settings.jsx
@@ -0,0 +1,12 @@
+import MetaTag from '../../hook/MetaTag/MetaTag'
+
+function Setting() {
+  return (
+    <>
+     <MetaTag title='Dashboard | Settings' />
+     this is setting
+    </>
+  )
+}
+
+export default Setting
--- frontend/dashboard/src/routes.jsx
+++ frontend/dashboard/src/routes.jsx
@@ -10,6 +10,7 @@ import Comments from "./page/Comments/Comments";
 import Inventory from "./page/Inventory/Inventory";
 import AddStock from "./page/AddStock/AddStock";
 import InventoryUpdate from "./page/InventoryUpdate/InventoryUpdate";
+import Settings from "./page/Settings/Settings";
 
 const routes = [
     {path:"/" , element:<Home/>},
@@ -24,6 +25,7 @@ const routes = [
     {path:"/Inventory" , element:<Inventory/>},
     {path:"/AddStock" , element:<AddStock/>},
     {path:"/InventoryUpdate" , element:<InventoryUpdate/>},
+    {path:"/Settings" , element:<Settings/>},
 ]
 
 export default routes
