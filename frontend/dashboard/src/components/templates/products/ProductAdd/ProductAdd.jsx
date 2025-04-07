2b20a65a0a6e4ba3a738939ce00a069cf8ae74f2
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-07 15:21:42 +0330

    Created section ProductAdd for products-page

---
+++ frontend/dashboard/src/components/templates/products/ProductAdd/ProductAdd.jsx
@@ -0,0 +1,47 @@
+import './ProductAdd.css'
+import {useTranslation} from 'react-i18next'
+
+function ProductAdd() {
+    const {t} =  useTranslation();
+  return (
+    <section className="addProduct box">
+        <div>
+            <h2 className="title_header">{t("Add Product")}</h2>
+            <form action="#" className="addProduct_form">
+                <div className="form_group">
+                    <label >{t("Product Name")}</label>
+                    <input type="text" className="form_input" />
+                </div>
+                <div className="form_group">
+                    <label>{t("Category")}</label>
+                    <select className='form_input'>
+                        <option value="">{t("Select Category")}</option>
+                        <option value="">{t("Electronics")}</option>
+                        <option value="">{t("Clothing")}</option>
+                        <option value="">{t("Books")}</option>
+                    </select>
+                </div>
+                <div className="form_group">
+                    <label>{t("Price")}</label>
+                    <input type="text" className="form_input" />
+                </div>
+                <div className="form_group">
+                    <label >{t("Stock")}</label>
+                    <input type="text" className="form_input" />
+                </div>
+                <div className="form_group">
+                    <label>{t("Description")}</label>
+                    <input type="text" className="form_input" />
+                </div>
+                <div className="form_group">
+                    <label >{t("Image")}</label>
+                    <input type="file"  className='form_input'/>
+                </div>
+                <button className="btn">{t("Save Product")}</button>
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default ProductAdd
--- frontend/dashboard/src/page/AddProduct/AddProduct.jsx
+++ frontend/dashboard/src/page/AddProduct/AddProduct.jsx
@@ -1,9 +1,11 @@
+import ProductAdd from '../../components/templates/products/ProductAdd/ProductAdd'
 import MeteTag from '../../hook/MetaTag/MetaTag'
 
 function AddProduct() {
   return (
     <>
     <MeteTag title="Dashboard | AddProduct"/>
+    <ProductAdd/>
     </>
   )
 }
