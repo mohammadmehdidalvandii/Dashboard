171729498eca851f1e5a9fd292946c2c37b0660e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-09 14:41:50 +0330

    update validation userModel

--- backend/src/validations/UserModelValidation.js
+++ backend/src/validations/UserModelValidation.js
@@ -3,7 +3,6 @@ const joi = require("joi");
 const userModelValidation = joi.object({
     firstName: joi.string()
         .min(3)
-        .max(10)
         .required(),
     lastName: joi.string()
         .min(10)
@@ -13,8 +12,7 @@ const userModelValidation = joi.object({
         .required()
         .default('ADMIN ADMIN'),
     image: joi.string()
-        .default("/images/default-image.png")
-        .required(),
+        .default("/images/default-image.png"),
     email: joi.string()
         .email()
         .required(),
@@ -22,11 +20,9 @@ const userModelValidation = joi.object({
         .pattern(new RegExp('^09\\d{9}$'))
         .required(),
     role: joi.string()
-        .required()
         .default("ADMIN"),
     password: joi.string()
         .min(8)
-        .max(24)
         .required()
         .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$')),
     refreshToken: joi.string(),
