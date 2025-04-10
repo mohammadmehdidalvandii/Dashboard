94ab940382121c0e20cd31cd6ade5d5c70eb295b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-10 17:55:21 +0330

    created file constants for statusCode

---
+++ backend/src/constants/constants.js
@@ -0,0 +1,21 @@
+const statusCodes = {
+    // Success codes
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+
+    // Client error codes
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+
+    // Server error codes
+    INTERNAL_SERVER_ERROR: 500,
+    BAD_GATEWAY: 502,
+    SERVICE_UNAVAILABLE: 503
+};
+
+module.exports = {
+    statusCodes
+};
