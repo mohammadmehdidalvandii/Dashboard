91d995664c5c52e023de7ebbc3fea5da90747625
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-12 15:51:14 +0330

    fixed productModel

--- backend/src/models/Products.js
+++ backend/src/models/Products.js
@@ -6,21 +6,22 @@ const schema = new mongoose.Schema({
     description:String,
     price:String,
     category:String,
-    image:[String],
+    image:String,
     stock:String,
     sku:String,
-    status:Boolean,
+    status:String,
     createdAt:Date,
     updatedAt:Date,
 });
 
+schema.pre("save", function(next) {
+    const { error } = productModelValidation.validate(this.Schema);
+    if (error) {
+        return next(new Error(error.details[0].message));
+    }
+    next();
+});
 
-schema.pre("save" , (next)=>{
-    const {error} = productModelValidation.validate(this.schema);
-    if(error) return next(error);
-    next()
-})
-
-const model = mongoose.models.Products || mongoose.model("Products" , schema);
+const model = mongoose.models.Products || mongoose.model("Products", schema);
 
 module.exports = model;
