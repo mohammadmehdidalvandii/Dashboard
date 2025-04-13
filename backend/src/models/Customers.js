d01fa460acca65baf688035ad8c46b1dc9f8d4f2
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-13 15:04:37 +0330

    created customersModel is fixed

---
+++ backend/src/models/Customers.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const customersModelValidation = require('../validations/CustomersModelValidation');
+
+const schema = new mongoose.Schema({
+    firstName:String,
+    lastName:String,
+    email:String,
+    phone:String,
+    address:{
+        street:String,
+        city:String,
+        state:String,
+        zipCode:String,
+        country:String
+    },
+    // orders:[]
+    membership:String,
+    note:String,
+    createdAt:Date,
+    updatedAt:Date
+})
+
+schema.pre("save" , function(next){
+    const  {error} = customersModelValidation.validate(this.schema);
+    if(error){
+        return next(new Error(error.details[0].message));
+    }
+    next();
+});
+
+
+
+const model = mongoose.models.Customers || mongoose.model("Customers" , schema);
+
+module.exports = model
---
+++ backend/src/validations/CustomersModelValidation.js
@@ -0,0 +1,13 @@
+const joi = require('joi');
+
+const customersModelValidation = joi.object({
+    firstName: joi.string().min(3).required(),
+    lastName: joi.string().min(4).required(),
+    email: joi.string().email().required(),
+    phone: joi.string().pattern(new RegExp('^09\\d{9}$')).required(),
+    address: joi.string().required(),
+    membership: joi.string().required(),
+    note: joi.string().allow('').optional(),
+});
+
+module.exports = customersModelValidation;
