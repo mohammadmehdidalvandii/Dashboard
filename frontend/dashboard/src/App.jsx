7bf2bafabe9f414fac5888b79d271bcc617385e0
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-20 16:23:41 +0330

    fixed app

--- frontend/dashboard/src/App.jsx
+++ frontend/dashboard/src/App.jsx
@@ -4,30 +4,20 @@ import Sidebar from "./components/modules/Sidebar/Sidebar";
 import MenuBar from "./components/modules/MenuBar/MenuBar";
 import Header from "./components/modules/Header/Header";
 import { HelmetProvider } from 'react-helmet-async';
-import Cookies from 'js-cookie';
 
 function App() {
-  const router = useRoutes(routes);
-
-  const token = Cookies.get("token");
-  
+  const router = useRoutes(routes);  
 
   return (
     <HelmetProvider>
       <div className="container">
-        {token  &&(
         <div className="container_sidebar">
           <Sidebar/>
         </div>
-        )}
         <div className="container_content">
-          {token && (
             <Header/>
-          )}
           {router}
-          {token && (
             <MenuBar/>
-          )}
         </div>
       </div>
     </HelmetProvider>
