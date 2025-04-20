6d9fba9d895acafd91db667e83952dc59b9d8d1e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-20 14:30:39 +0330

    created Login page

---
+++ frontend/dashboard/src/page/Login/Login.jsx
@@ -0,0 +1,13 @@
+import MetaTag from "../../hook/MetaTag/MetaTag"
+
+
+function Login() {
+  return (
+    <>
+      <MetaTag title="Dashboard || Login"/>
+      this is Login page
+    </>
+  )
+}
+
+export default Login
