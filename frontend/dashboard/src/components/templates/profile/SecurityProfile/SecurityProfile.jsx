33520964df3e949c0595b514a9df03aa6158235a
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-04-29 16:02:27 +0330

    update profile showAlert

--- frontend/dashboard/src/components/templates/profile/SecurityProfile/SecurityProfile.jsx
+++ frontend/dashboard/src/components/templates/profile/SecurityProfile/SecurityProfile.jsx
@@ -1,8 +1,13 @@
 import './SecurityProfile.css'
 import {useTranslation} from 'react-i18next'
+import showAlert from '../../../../utils/showAlert'
 
 function SecurityProfile() {
-    const {t} = useTranslation()
+    const {t} = useTranslation();
+    const handlerChangePassword = (e)=>{
+        e.preventDefault()
+        showAlert("This feature is not enabled","warning","ok")
+    }
   return (
     <section className="securityProfile box">
         <div>
@@ -20,7 +25,7 @@ function SecurityProfile() {
                     <label htmlFor="">{t("Confirm New Password")}</label>
                     <input type="text" className="form_input" placeholder={t("Confirm new password")}/>
                 </div>
-                <button className="btn">{t("Update Password")}</button>
+                <button className="btn" onClick={handlerChangePassword}>{t("Update Password")}</button>
             </form>
         </div>
     </section>
